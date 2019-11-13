7ad7cb7612fca93e9f6b9dcb4c63fdd6f4fb7863
Florian Perez <florian.perez@iut-rodez.fr>
2019-11-13 11:16:46 +0100

    fix #3.1 Filtre sur status et username

--- all_users.php
+++ all_users.php
@@ -17,11 +17,20 @@
 		throw new PDOException($e->getMessage(),(int)$e->getCode());
 	}
 	
+	$lettreUsername = 'e%';
 	echo "<table>";
 	echo "<tr><th>Id</th><th>Username</th><th>Email</th><th>Status</th></tr>";
-	$stmt=$pdo->query('SELECT * FROM users');
+	$stmt=$pdo->query("SELECT * FROM users WHERE username LIKE '$lettreUsername' AND status_id=2");
 	while($row=$stmt->fetch()){
-		echo "<tr><td>".$row['id']."</td>"."<td>".$row['username']."</td>"."<td>".$row['email']."</td>"."<td>".$row['status_id']."</td></tr>";
+		echo "<tr><td>".$row['id']."</td>"."<td>".$row['username']."</td>"."<td>".$row['email']."</td>"."<td>";
+		if ($row['status_id'] == 1) {
+			echo 'Waiting for account confirmation';
+		} else if ($row['status_id'] == 2) {
+			echo 'Active account';
+		} else if ($row['status_id'] == 3) {
+			echo 'Waiting for account deletion';
+		}			
+		echo "</td></tr>";
 	}
 	echo "</table>";
 ?>
